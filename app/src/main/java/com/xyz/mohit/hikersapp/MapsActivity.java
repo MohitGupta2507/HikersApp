1dac6043bd2881fc4a6a5b258e69bff55502731c
Mohit <mg25071998@gmail.com>
2018-11-28 13:27:42 +0530

    The Final Version

--- app/src/main/java/com/xyz/mohit/hikersapp/MapsActivity.java
+++ app/src/main/java/com/xyz/mohit/hikersapp/MapsActivity.java
@@ -17,6 +17,7 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.view.KeyEvent;
 import android.view.View;
@@ -55,6 +56,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     Button button;
     LocationManager locationManager;
     LocationListener locationListener;
+    Criteria criteria;
     BottomSheetBehavior bottomSheetBehavior;
     Double Longg, latii;
 
@@ -68,32 +70,31 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         bottomSheetBehavior.setPeekHeight(120);
         bottomSheetBehavior.setHideable(false);
         locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        button=(Button)findViewById(R.id.button);
-        SearchText=(AutoCompleteTextView) findViewById(R.id.Search);
+        button = (Button) findViewById(R.id.button);
+        SearchText = (AutoCompleteTextView) findViewById(R.id.Search);
         // On Search Initiated
         GeoDataClient mGoogleApiClient;
-        mGoogleApiClient = Places.getGeoDataClient(this,null);
+        mGoogleApiClient = Places.getGeoDataClient(this, null);
 
-        LatLngBounds latLng=new LatLngBounds(new LatLng(23.63936, 68.14712), new LatLng(28.20453, 97.34466));
+        LatLngBounds latLng = new LatLngBounds(new LatLng(23.63936, 68.14712), new LatLng(28.20453, 97.34466));
 
         AutocompleteFilter filter = new AutocompleteFilter.Builder()
                 .setTypeFilter(AutocompleteFilter.TYPE_FILTER_CITIES)
                 .build();
-        PlaceAutocompleteAdapter adapter=new PlaceAutocompleteAdapter(MapsActivity.this,mGoogleApiClient,latLng,null);
+        PlaceAutocompleteAdapter adapter = new PlaceAutocompleteAdapter(MapsActivity.this, mGoogleApiClient, latLng, null);
         SearchText.setAdapter(adapter);
 
         SearchText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView textView, int actionId, KeyEvent keyEvent) {
 
-                if(actionId == EditorInfo.IME_ACTION_SEARCH
+                if (actionId == EditorInfo.IME_ACTION_SEARCH
                         || actionId == EditorInfo.IME_ACTION_DONE
                         || keyEvent.getAction() == keyEvent.ACTION_DOWN
-                        || keyEvent.getAction() == keyEvent.KEYCODE_ENTER)
-                {
+                        || keyEvent.getAction() == keyEvent.KEYCODE_ENTER) {
 
-                    View v=MapsActivity.this.getCurrentFocus();
-                    if(v != null) {
+                    View v = MapsActivity.this.getCurrentFocus();
+                    if (v != null) {
 
 
                         InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
@@ -108,12 +109,82 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             }
         });
 
-       // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
         //      checkPlayServices();
 
+
+        Toast.makeText(MapsActivity.this, "Please Wait till Location Updates", Toast.LENGTH_LONG).show();
+
+    }
+
+
+    private void geoLocate() {
+
+        mMap.clear();
+
+
+        String SearchT = SearchText.getText().toString();
+
+
+        Geocoder geocoder = new Geocoder(getApplicationContext());
+
+        Address address = null;
+
+        List<Address> list = new ArrayList<Address>();
+
+        try {
+            list = geocoder.getFromLocationName(SearchT, 1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        if (list != null && list.size() > 0) {
+
+            address = list.get(0);
+            String add = "";
+            if (list.get(0).getSubThoroughfare() != null) {
+                add += list.get(0).getSubThoroughfare() + " , ";
+            }
+
+            if (list.get(0).getThoroughfare() != null) {
+                add += list.get(0).getThoroughfare() + " , ";
+            }
+            if (list.get(0).getLocality() != null) {
+                add += list.get(0).getLocality() + " , ";
+            }
+            if (list.get(0).getCountryName() != null) {
+                add += list.get(0).getCountryName();
+            }
+
+            Toast.makeText(MapsActivity.this, add.toString(), Toast.LENGTH_LONG);
+            LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
+            mMap.clear();
+            mMap.addMarker(new MarkerOptions().position(latLng).title("New Location"));
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+            button.setVisibility(View.VISIBLE);
+            updateui(add.toString(), address.getLatitude(), address.getLongitude());
+
+        }
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        mMap=googleMap;
+        mMap.clear();
         locationListener = new LocationListener() {
             @Override
             public void onLocationChanged(Location location) {
@@ -122,7 +193,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 mMap.clear();
                 LatLng sydney = new LatLng(latii, Longg);
                 mMap.addMarker(new MarkerOptions().position(sydney).title("Your Location"));
-                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 20));
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
 
                 Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
 
@@ -133,12 +204,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                         String address = "";
 
                         if (listAdress.get(0).getSubThoroughfare() != null) {
-                            address += listAdress.get(0).getSubThoroughfare()+" , ";                      }
+                            address += listAdress.get(0).getSubThoroughfare() + " , ";
+                        }
 
                         if (listAdress.get(0).getThoroughfare() != null) {
-                            address += listAdress.get(0).getThoroughfare()+" , ";                        }
+                            address += listAdress.get(0).getThoroughfare() + " , ";
+                        }
                         if (listAdress.get(0).getLocality() != null) {
-                            address += listAdress.get(0).getLocality()+" , ";
+                            address += listAdress.get(0).getLocality() + " , ";
                         }
                         if (listAdress.get(0).getCountryName() != null) {
                             address += listAdress.get(0).getCountryName();
@@ -172,81 +245,25 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             }
         };
 
-        Toast.makeText(MapsActivity.this,"Please Wait till Location Updates",Toast.LENGTH_LONG).show();
-
-    }
 
 
-    private void geoLocate() {
 
-        mMap.clear();
-
-
-        String SearchT=SearchText.getText().toString();
-
-
-        Geocoder geocoder=new Geocoder(getApplicationContext());
-
-        Address address = null;
-
-        List<Address> list= new ArrayList<Address>();
-
-        try {
-            list =geocoder.getFromLocationName(SearchT,1);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        if (list != null && list.size() > 0) {
-
-            address=list.get(0);
-            String add="";
-            if (list.get(0).getSubThoroughfare() != null) {
-                add += list.get(0).getSubThoroughfare()+" , ";                      }
-
-            if (list.get(0).getThoroughfare() != null) {
-                add += list.get(0).getThoroughfare()+" , ";                        }
-            if (list.get(0).getLocality() != null) {
-                add += list.get(0).getLocality()+" , ";
-            }
-            if (list.get(0).getCountryName() != null) {
-                add += list.get(0).getCountryName();
-            }
-
-            Toast.makeText(MapsActivity.this,add.toString(),Toast.LENGTH_LONG);
-            LatLng latLng=new LatLng(address.getLatitude(),address.getLongitude());
-            mMap.clear();
-            mMap.addMarker(new MarkerOptions().position(latLng).title("New Location"));
-            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 20));
-            button.setVisibility(View.VISIBLE);
-            updateui(add.toString(),address.getLatitude(),address.getLongitude());
-
-        }
-    }
-
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
 
         if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
             buildAlertMessageNoGps();
         } else {
 
-            Criteria criteria=new Criteria();
-            criteria.setPowerRequirement(criteria.ACCURACY_MEDIUM);
-            criteria.setSpeedAccuracy(criteria.ACCURACY_HIGH);
+            criteria = new Criteria();
+            criteria.setPowerRequirement(criteria.POWER_LOW);
+            criteria.setSpeedAccuracy(criteria.ACCURACY_LOW);
+            criteria.setSpeedRequired(false);
+            criteria.setAltitudeRequired(false);
+            criteria.setBearingAccuracy(0);
+            criteria.setCostAllowed(true);
+            criteria.setHorizontalAccuracy(criteria.ACCURACY_HIGH);
+            criteria.setVerticalAccuracy(criteria.ACCURACY_MEDIUM);
+            criteria.setAccuracy(criteria.ACCURACY_FINE);
 
-            mMap = googleMap;
-            mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
 
             // Add a marker in Sydney and move the camera
             if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
@@ -254,19 +271,18 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 return;
             } else {
 
-                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000,20 , locationListener);
-                l= locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                locationManager.requestLocationUpdates(0,5,criteria,locationListener,null);
+                l = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
             }
-            if(l==null)
-            {
-                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 20, locationListener);
+            if (l == null) {
+                locationManager.requestLocationUpdates(0,5,criteria,locationListener,null);
+//                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 5, locationListener);
 
-            }
-            else {
+            } else {
                 mMap.clear();
                 LatLng sydney = new LatLng(l.getLatitude(), l.getLongitude());
                 mMap.addMarker(new MarkerOptions().position(sydney).title("Your Location"));
-                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 20));
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney,17));
                 updateui("Your Current Address", l.getLatitude(), l.getLongitude());
             }
             mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
@@ -275,7 +291,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                     button.setVisibility(View.VISIBLE);
                     mMap.clear();
                     mMap.addMarker(new MarkerOptions().position(latLng).title("New Location"));
-                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 20));
+                    mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
 
                     Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
 
@@ -285,18 +301,20 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                         if (listAdress != null && listAdress.size() > 0) {
                             String address = "";
                             if (listAdress.get(0).getSubThoroughfare() != null) {
-                                address += listAdress.get(0).getSubThoroughfare()+" , ";                      }
+                                address += listAdress.get(0).getSubThoroughfare() + " , ";
+                            }
 
                             if (listAdress.get(0).getThoroughfare() != null) {
-                                address += listAdress.get(0).getThoroughfare()+" , ";                        }
+                                address += listAdress.get(0).getThoroughfare() + " , ";
+                            }
                             if (listAdress.get(0).getLocality() != null) {
-                                address += listAdress.get(0).getLocality()+" , ";
+                                address += listAdress.get(0).getLocality() + " , ";
                             }
                             if (listAdress.get(0).getCountryName() != null) {
                                 address += listAdress.get(0).getCountryName();
                             }
 
-                            updateui(address, latLng.latitude,latLng.longitude);
+                            updateui(address, latLng.latitude, latLng.longitude);
 
                         }
                     } catch (IOException e) {
@@ -328,7 +346,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
             if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
                 if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 20, locationListener);
+                    locationManager.requestLocationUpdates(0,5,criteria,locationListener,null);
                     l = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
 
 
@@ -344,6 +362,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
                     public void onClick(@SuppressWarnings("unused") final DialogInterface dialog, @SuppressWarnings("unused") final int id) {
                         startActivity(new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+                        if (ActivityCompat.checkSelfPermission(MapsActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)                 locationManager.requestLocationUpdates(0,5,criteria,locationListener,null);
+                        locationManager.requestLocationUpdates(0,5,criteria,locationListener,null);
+
                     }
 
                 })
@@ -356,17 +377,33 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         alert.show();
     }
 
-    public void RecenterClicked(View v)
-    {
+    public void RecenterClicked(View v) {
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
 
-                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 20, locationListener);
-                button.setVisibility(View.GONE);
-            }
+            locationManager.requestLocationUpdates(0,5,criteria,locationListener,null);
+            button.setVisibility(View.GONE);
+        }
     }
 
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
 
     }
+
+   /* @Override
+    protected void onStart() {
+        super.onStart();
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            {/*
+
+                if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+                    buildAlertMessageNoGps();
+                } else {
+                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+                }
+            }
+
+        }
+    }*/
 }
+
